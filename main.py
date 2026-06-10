8b354d8d173965b9d99fb4cb5596d4da681a7d70
hfiwahdijwaiskdljwali <fratiasf@gmail.com>
2026-06-10 14:41:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def make_bot(token):
         await ctx.send("🛑 Bump task stopped.")
 
     ##@tasks.loop(hours=2, minutes=1)
-    @tasks.loop(secounds=10)
+    @tasks.loop(seconds=10)
     async def repeat():
         await trigger_command()
 
