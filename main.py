423325e6702a67db8e6aa1cf35678528e53b81be
dilxwazi <clemensiuscomminius@outlook.de>
2026-08-24 23:02:45 +0300

    main.py aktualisieren

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def make_bot(token):
                 if resp.status == 204:
                     print(f"✅ [{token[:10]}...] Triggered in guild {guild_id}")
                 else:
+                    # Nur noch die Fehlernummer anzeigen, kein HTML-Text mehr
                     print(f"❌ [{token[:10]}...] Failed in guild {guild_id}: {resp.status}")
-                    print(await resp.text())
 
     @bot.event
     async def on_ready():
